4627cbd086022ee992f9d5d84eeb23261bb5038d
Syed Mohd Umair <Syed.Umair@gds.ey.com>
2020-07-12 15:49:08 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,14 @@
-console.log('Client side javascript file is loaded!')
+//console.log('Client side javascript file is loaded!')
+const weatherForm = document.querySelector('form') 
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
         console.log(data)
     })
 })
-
-
-
-const weatherForm = document.querySelector('form') 
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
